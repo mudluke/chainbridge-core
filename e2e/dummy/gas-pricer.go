f7b5ff62c47bd251e25b66463f5d88404d116011
Luke <luchenqun@qq.com>
2024-08-08 17:03:09 +0800

    gasprice set priority+suggest

--- e2e/dummy/gas-pricer.go
+++ e2e/dummy/gas-pricer.go
@@ -1,6 +1,7 @@
 package dummy
 
 import (
+	"context"
 	"math/big"
 
 	"github.com/ChainSafe/chainbridge-core/chains/evm/calls/evmgaspricer"
@@ -21,16 +22,22 @@ func NewStaticGasPriceDeterminant(client evmgaspricer.GasPriceClient, opts *evmg
 
 func (gasPricer *StaticGasPriceDeterminant) GasPrice(priority *uint8) ([]*big.Int, error) {
 	var gasPrice []*big.Int
+
+	suggest, err := gasPricer.client.SuggestGasPrice(context.TODO())
+	if err != nil {
+		return nil, err
+	}
+
 	switch *priority {
 	// slow
 	case 0:
-		gasPrice = []*big.Int{big.NewInt(50000000000)}
+		gasPrice = []*big.Int{big.NewInt(0).Add(suggest, big.NewInt(50000000000))}
 	// fast
 	case 2:
-		gasPrice = []*big.Int{big.NewInt(140000000000)}
+		gasPrice = []*big.Int{big.NewInt(0).Add(suggest, big.NewInt(140000000000))}
 	// medium
 	default:
-		gasPrice = []*big.Int{big.NewInt(80000000000)}
+		gasPrice = []*big.Int{big.NewInt(0).Add(suggest, big.NewInt(80000000000))}
 	}
 	return gasPrice, nil
 }
